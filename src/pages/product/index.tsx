3fa6a296e5ccffc8326a424f04e6ea5fa05e9023
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-05-15 22:18:02 -0300

    feat(product): implements file logical to get jpg, jpeg & png

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,8 +2,30 @@ import { Header } from "@/components/Header";
 import { canSSRAuth } from "@/utils/casSSRAuth";
 import Head from "next/head";
 import styles from './styles.module.scss';
+import { FiUpload } from 'react-icons/fi'
+import { ChangeEvent, useState } from "react";
 
 export default function Product() {
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  function handleFile(evt: ChangeEvent<HTMLInputElement>) {
+    if (!evt.target.files) {
+      return;
+    }
+
+    const image = evt.target.files[0];
+
+    if (!image) {
+      return;
+    }
+
+    if (image.type === 'image/jpeg' || image.type === 'image/png') {
+      setImageAvatar(image)
+      setAvatarUrl(URL.createObjectURL(image));
+    }
+  }
+
   return (
     <>
       <Head>
@@ -16,7 +38,30 @@ export default function Product() {
           <h1>Novo produto</h1>
 
           <form className={styles.form}>
-            
+
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={30} />
+              </span>
+
+              <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={handleFile}
+              />
+
+              {avatarUrl && (
+                  <img
+                    className={styles.preview}
+                    src={avatarUrl}
+                    alt="Foto do produto"
+                    width={250}
+                    height={250}
+                  />
+              )}
+
+            </label>
+
             <select name="" id="">
               <option>
                 Bebida
