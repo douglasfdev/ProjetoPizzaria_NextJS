e98db00de00288b6954615b2e66f7a3745b94d4e
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-05-23 21:28:06 -0300

    feat(dashboard): init and stylized the dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,18 +1,40 @@
 import { Header } from '@/components/Header';
 import { canSSRAuth } from '@/utils/casSSRAuth';
 import Head from 'next/head';
+import styles from './style.module.scss';
+import { FiRefreshCcw } from 'react-icons/fi';
 
 export default function Dashboard() {
-  return(
+  return (
     <>
       <Head>
         <title> Painel - Tech Pizza </title>
       </Head>
 
-      <main>
+      <div>
         <Header />
-        <h1>Painel</h1>
-      </main>
+
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Últimos Pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} />
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+
+            <section className={styles.orderItem}>
+              <button>
+                <div className={styles.tag}></div>
+                <span>Mesa 30</span>
+              </button>
+            </section>
+
+          </article>
+
+        </main>
+      </div>
     </>
   );
 }
